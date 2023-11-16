b789f57d5e50b9c18a4cb10d38fc0b742ad86fee
nkahambu <150119046+nkahambu@users.noreply.github.com>
2023-11-16 18:47:36 +0200

    Add files via upload
    first commit

---
+++ test1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string nom ="Nathalie";
+	int chiffre=34;
+	
+	cout<< "le nom est: "<<nom <<endl;
+	cout<<"la valeur est:"<<chiffre;
+}
